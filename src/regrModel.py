2ce443f0f77b49baf46dbf22415e697e5d51e4fc
Felicia <felicia.burtscher@hotmail.com>
2018-04-15 00:33:22 +0100

    added adjusted plot

--- src/regrModel.py
+++ src/regrModel.py
@@ -143,7 +143,7 @@ plt.title('Coefficients of linear regression model')
 #plot_url = py.plot_mpl(fig, filename='mpl-basic-bar')
 plt.gcf().subplots_adjust(bottom=0.5)
 plt.show()
-plt.savefig('img/coefficients.png')
+#plt.savefig('img/coefficients.png')
 plt.close(fig)
 
 
